6fb26fc15de87ef173c0093561c3ffc210abc7f4
aajitverma001 <ajit@chargebee.com>
2013-06-13 16:02:31 +0530

    Class Name changes

--- jquery-ui-timepicker-addon.js
+++ jquery-ui-timepicker-addon.js
@@ -1078,13 +1078,13 @@
                 },
                 options: function(tp_inst, obj, unit, opts, val){
                     if(typeof(opts) == 'string' && val !== undefined)
-                        return obj.find('.ui-timepicker-input').spinner(opts, val);
-                    return obj.find('.ui-timepicker-input').spinner(opts);
+                        return obj.find('.ui-timepicker-spinner').spinner(opts, val);
+                    return obj.find('.ui-timepicker-spinner').spinner(opts);
                 },
                 value: function(tp_inst, obj, unit, val){
                     if(val !== undefined)
-                        return obj.find('.ui-timepicker-input').spinner('value', val);
-                    return obj.find('.ui-timepicker-input').spinner('value');
+                        return obj.find('.ui-timepicker-spinner').spinner('value', val);
+                    return obj.find('.ui-timepicker-spinner').spinner('value');
                 }
             }
         } // end _controls
